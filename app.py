3883798be00936f9cbeaa8482cb9d3e5c1514d55
shubhayu-64 <majumdarshubhayu@gmail.com>
2023-03-04 02:53:25 +0530

    Build basic webapp

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template, request
-import pandas as pd
 from YouTube_Scraper import search_youtube
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         search_term = request.form['search_term']
         results = search_youtube(search_term)
-        res=results.to_dict('dict')
+        res = results.to_dict('dict')
 
         print(res)
         return render_template('results.html', results=res)
     else:
         return render_template('index.html')
 
-if __name__=="__main__":
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host='0.0.0.0')
