23670822973c4168486a9e66949e5205b8555ead
nilesh-sengupta <nileshsengupta43@gmail.com>
2023-03-08 13:58:52 +0530

    New version

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template, request
-from YouTube_Scraper import search_youtube
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect
+import pandas as pd
+import scraper
+import multiprocessing
+from scraper import scraper
 
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        search_term = request.form['search_term']
-        results = search_youtube(search_term)
-        res = results.to_dict('dict')
-
-        print(res)
+        search_term= request.form['search_term']
+        scroll_num = request.form['scroll_num']
+        num_vid = request.form['num_vid']
+        multiprocessing.freeze_support() 
+        search_results = scraper(search_term,int(num_vid),int(scroll_num))
+        results = search_results.main()
+        res=results.to_dict('dict')
+        res['num_vid']=int(num_vid)
         return render_template('results.html', results=res)
     else:
         return render_template('index.html')
 
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='0.0.0.0')
+if __name__=="__main__":
+    app.run(debug=True)
